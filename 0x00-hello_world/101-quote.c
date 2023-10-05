42a5d3d16ac50d8eb45c05f7ba77b4ac6bf45a90
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-05 09:26:33 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <unistd.h>
 
 /**
- * main  - entry point
+ * main - Entry point
  *
- * Description: program entry point
- *
- * Return: always failure (1)
+ * Return: Always 1 (Error)
  */
+int main(void)
+{
+	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-
-int main(void) {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;  // Return a non-zero value to indicate an error
+	write(2, m, sizeof(m) - 1);
+	return (1);
 }
 
