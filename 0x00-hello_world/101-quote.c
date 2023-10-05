ab192e583c34bc72b7d3e37dde63f2ca22421e30
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-05 09:27:30 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, m, sizeof(m) - 1);
 	return (1);
