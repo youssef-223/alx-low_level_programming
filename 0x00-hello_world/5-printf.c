fece9ca5f2a40ede162fce91ba103eb6beadfeb1
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-05 08:45:30 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
