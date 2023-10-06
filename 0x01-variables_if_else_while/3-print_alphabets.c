e50dcddd09a5cbb7acde10ebf1148fa9d3535c3f
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-06 11:40:14 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+int main(void)
+{
+		char n = 'a';
+
+		while (n <= 'z')
+		{
+			putchar(n);
+			n++;
+		}
+		putchar('\n');
+
+		while (N <= 'Z')
+		{
+			putchar(N);
+			N++;
+		}
+		return (0);
+}
