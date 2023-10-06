97421d4fd207314db2f2f9e5f58b6380a3f3e68b
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-06 11:49:33 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+int main(void)
+{
+	char n = 'a';
+
+		while (n <= 'z')
+		{
+			if (n == 'q' || n == 'e')
+					n++;
+			putchar(n);
+			n++;
+		}
+		putchar('\n');
+	return (0);
+}
