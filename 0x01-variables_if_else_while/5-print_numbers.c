e4b8126222f6531fc03523e42073ff419d8f6271
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-06 12:02:29 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+int main(void)
+{
+	int n = '1';
+
+		while (n <= 9)
+		{
+			printf("%d", n);
+			n++;
+		}
+		putchar('\n');
+	return (0);
+}
