9d26aa085acae01986a73dfaf7fc8ef29c049ad9
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-06 12:21:59 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+int main(void)
+{
+	int n = 0;
+
+		while (n <= 9)
+		{
+			putchar(n + '0');
+
+			if (n != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			{
+			n++;
+		}
+		putchar('\n');
+	return (0);
+}
