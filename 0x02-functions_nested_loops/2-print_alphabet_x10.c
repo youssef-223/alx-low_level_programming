6aa3e4e3f5a3903eb617f64d23e2faedb8214ce0
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-10 07:17:12 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - print_alphabet_x10
+ *
+ * Return: on success (0)
+ *      on failure (1)
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+		_putchar(ch);
+	_putchar('\n');
+}
