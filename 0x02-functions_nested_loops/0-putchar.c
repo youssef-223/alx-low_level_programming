0fcde4a7308d845edf15ca3e7cfba7b284934b7c
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-10 06:24:33 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
+
