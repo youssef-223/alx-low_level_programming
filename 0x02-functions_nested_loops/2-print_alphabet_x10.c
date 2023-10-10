f212a7819cebec41f7f8bae8a315d691294e5af1
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-10 07:24:53 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,6 @@
 
 /**
  * print_alphabet_x10 - print_alphabet_x10
- *
- * Return: on success (0)
- *      on failure (1)
  */
 
 void print_alphabet_x10(void)
@@ -14,6 +11,10 @@ void print_alphabet_x10(void)
 	int line, ch;
 
 	for (line = 0; line <= 9; line++)
-		_putchar(ch);
-	_putchar('\n');
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+
 }
