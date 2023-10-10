65c857a8103df610a35aa64b29860629ccb39b9b
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-10 07:43:24 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_sign - print_sign
+ *@n: paramter
+ *
+ * Return: 1 if error
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+		_putchar(45);
+		return (-1);
+	}
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _abs - _abs
+ *@n: paramter
+ *
+ * Return: 1 if error
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
