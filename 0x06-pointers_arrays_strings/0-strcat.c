99713f384cd49639033a37605b1c5824bb3970a1
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-19 02:57:34 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,21 +12,18 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j = 0;
 
-	while (src[i] != '\0')
-	{
+	while (dest[i])
 		i++;
-	}
 
-	while (dest[j] != '\0')
-	{
-		src[i] = dest[j];
+	while (src[j] != '\0')
+	{	
+		dest[i++] = src[j];
 
-		i++;
 		j++;
 	}
 
-	src[i] = '\0';
+	src[++i] = '\0';
 
-	return (dest);
+	return (src);
 }
 
