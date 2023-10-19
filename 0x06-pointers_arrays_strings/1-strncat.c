735168a97db1da7d7ead685e62fc086025312a13
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-19 03:33:56 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * * _strncat - Prints the elements of an array
+ * * _strncpy - Prints the elements of an array
  * @dest: param1
  * @src: param2
  * @n: param3
  * Description: copy the string to  by pointer src to
  *Return: pointer to dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, j;
 
 	while (dest[i])
 		i++;
 
-	for (j = 0; j <= n; j++)
+	for (j = 0; j <= n && src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 
