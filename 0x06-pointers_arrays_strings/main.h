677d34adcf9485ed941da43d5fe72a063b048895
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-19 03:29:47 -0700

    a

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+
 #endif
