ba28e700154c9150c6f6230f66913d2902970513
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-19 03:53:17 -0700

    a

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * * _strcmp - Prints the elements of an array
+ * @s1: param1
+ * @s2: param2
+ * Description: copy the string to  by pointer src to
+ * Return: pointer to dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i] ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (0);
+		}
+
+	}
+	return (1);
+}
+
