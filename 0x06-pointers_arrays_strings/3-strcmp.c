35046ae966d710623f1d4c7ad4ef85979d1faceb
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-19 04:08:04 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int equal = 0;
 
-	for (i = 0; s1[i] && s2[i] ; i++)
+	while (*s1)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
-			return (1);
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
 		}
-
+		s1++;
+		s2++;
 	}
 
-	return (0);
+	return (equal);
 }
 
