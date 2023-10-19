b3bfd49ab31cd742f9d5f54a27ae1315b9fae166
yossef-223 <youssef.moustafa202@gmail.com>
2023-10-19 02:50:40 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * * _strcat - Prints the elements of an array
+ * @dest: param1
+ * @src: param2
+ * Description: copy the string to  by pointer src to
+ *Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src);
+{
+	int i, j = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	while (dest[j] != '\0')
+	{
+		src[i] = dest[j];
+
+		i++;
+		j++;
+	}
+
+	src[i] = '\0';
+
+	return (src);
+}
+
