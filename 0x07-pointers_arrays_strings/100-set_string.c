24ceffef87adec9175388a2df6f7b7d7658840ab
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-23 12:35:03 -0700

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_string - Short description, single line
+ * @s: param1
+ * @to: param2
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
