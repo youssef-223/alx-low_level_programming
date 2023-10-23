17a401ac9c4dec83d4a788e6c1afa7c451210b32
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-23 12:12:41 -0700

    a

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - Short description, single line
+ * @a: param1
+ *
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
