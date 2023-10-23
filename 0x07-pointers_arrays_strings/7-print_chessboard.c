aecd459980723bcea28ff2a18c5dc1b5cc39f76a
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-23 12:28:23 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * print_chessboard - Short description, single line
  * @a: param1
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - Short description, single line
+ * @a: param1
+ * @size: param2
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int s1, s2;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+
+	_putchar("%d, ", s1);
+	_putchar("%d\n", s2);
+}
