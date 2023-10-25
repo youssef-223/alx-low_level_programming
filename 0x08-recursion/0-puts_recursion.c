efe92b6087013ab18a8c9b5d18203354e70b586c
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-25 12:39:17 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - Short description, single line
+ * @s: param1
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+
+void _puts_recursion(char *s)
+{
+	while (s)
+	{
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+#includ <stdio.h>
+
+/**
+ * _putchar - writes character to standart output
+ *@c: The character to be written
+ *
+ * Return: on success (0)
+ *	on failure (1)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
