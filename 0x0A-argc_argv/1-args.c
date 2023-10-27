709fc049e452be9ee958af3ce9f5f703d4b2da3d
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-27 15:11:22 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - Short description, single line
+ * @argc: param1
+ * @argv: param2
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+int main(int argc, char const *argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
+
