023946a47c1fa452eb2b8f2a9ec4720b05f988ff
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-27 15:16:04 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,7 @@
  *
  * Return: Return
  */
-int main(int argc, char const *argv)
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,7 +10,7 @@
  *
  * Return: Return
  */
-int main(int argc, char const *argv)
+int main(int argc, char const *argv[])
 {
 	(void)argv;
 	printf("%d\n", argc);
