930afe7e6abb9a7a661d539e2134cd3ca8b77155
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-27 15:49:13 -0700

    a

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+/**
+ * main - Short description, single line
+ * @argc: param1
+ * @argv: param2
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, leastcents = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+	for (i = 0; i < 5; i++)
+	{
+		if (money <= cents[i])
+		{
+			leastcents += money / cents[i];
+			money = money % cents[i];
+			if (money % cents[i] == 0)
+			{
+				break;
+			}
+		}
+	}
+	printf("%d\n", leastcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
