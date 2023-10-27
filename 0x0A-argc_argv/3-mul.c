cdb32a44205eed8b0c1aef69a7c881b3f08d5b9e
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-27 15:26:25 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+/**
+ * main - Short description, single line
+ * @argc: param1
+ * @argv: param2
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+int main(int argc, char const *argv)
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sun = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - Short description, single line
+ * @argc: param1
+ * @argv: param2
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+int main(int argc, char const *argv)
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc] ; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+			sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
