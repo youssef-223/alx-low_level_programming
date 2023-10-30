674e2253f03d5322488509e82c258b16547a288d
youssef-223 <youssef.moustafa202@gmail.com>
2023-10-30 05:20:39 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,10 +35,10 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
