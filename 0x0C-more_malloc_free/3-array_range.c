8a20c3b9e7b71877a4653a6177b93cd6b2c4f3a9
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-03 09:59:43 -0700

    a

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,11 +17,17 @@ int *array_range(int min, int max)
 	int *ptr;
 
 	if (min > max)
+	{
 		return (NULL);
+	}
 	len = max - min + 1;
 	if (!ptr)
+    {
 		return (NULL);
+	}
 	for (i = 0; i < len; i++)
+	{
 		ptr[i] = min++;
+	}
 	return (ptr);
 }
