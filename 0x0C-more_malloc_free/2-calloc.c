dc66b9016d4f87fa6e8025f4908b9df036e59d85
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-03 09:51:27 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,22 +11,22 @@
  *
  * Return: Return
  */
- 
- 
- char *_memset(char *s, char b, unsigned int n)
- {
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
 	char *ptr = s;
 
 	while (n--)
+	{
 		*s++ = b;
-
+	}
 	return (ptr);
- }
+}
 /**
- * string_nconcat - Short description, single line
- * @s1: param1
- * @s2: param2
- * @n: param3
+ * _calloc - Short description, single line
+ * @nmemb: param1
+ * @size: param2
  * Description: Description
  *
  * Return: Return
@@ -36,14 +36,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *m;
 
-	if (size ==0 || nmemb ==0)
+	if (size == 0 || nmemb == 0)
+	{
 		return (NULL);
+	}
 	m = malloc(sizeof(int) * nmemb);
 
-	if ( m == 0)
+	if (m == 0)
 		return (NULL);
 
-	_memset(m, 0, sizeof(int) * nmemb);
+		_memset(m, 0, sizeof(int) * nmemb);
 
 	return (m);
 }
