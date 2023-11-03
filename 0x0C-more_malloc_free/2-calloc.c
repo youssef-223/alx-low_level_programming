33a7c4b3fcdbbf2b33324b8c554a85376fa39f33
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-03 10:07:22 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -43,9 +43,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	m = malloc(sizeof(int) * nmemb);
 
 	if (m == 0)
+	{
 		return (NULL);
-
-		_memset(m, 0, sizeof(int) * nmemb);
+	}
+	_memset(m, 0, sizeof(int) * nmemb);
 
 	return (m);
 }
