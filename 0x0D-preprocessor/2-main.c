4e808e278efd52286c1cc22007b6f87748cda1c3
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-06 15:08:07 -0800

    a

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main  - entry point
+ *
+ * Description: program entry point
+ *
+ * Return: alwasy success (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
