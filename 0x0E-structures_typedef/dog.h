c15ba0c4bb2a23ca20f758517e6528352822a0f7
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-06 15:26:21 -0800

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name : name
+ * @age : age
+ * @owner : owner
+ *
+ * Description: Description
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H  */
