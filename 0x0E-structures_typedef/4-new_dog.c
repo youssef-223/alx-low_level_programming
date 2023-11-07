c33edcd2bb9789d2c5445d8ed409ce37786f38ab
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-06 16:07:10 -0800

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ int _strlen(const char *str)
 
 /**
  * _strcpy - Short description, single line
- * @str: param1
+ * @src: param1
  * @dest: param1
  * Description: Description
  *
@@ -68,7 +68,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	
+
 	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if ((*dog).owner == NULL)
 	{
