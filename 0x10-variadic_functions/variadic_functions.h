bc74b929fb1fbbea84bfc846d82841c531fc8093
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-09 15:33:20 -0800

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@ typedef struct token
 {
 	char *token;
 	void (*f)(char *, va_list);
-}token_t;
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
