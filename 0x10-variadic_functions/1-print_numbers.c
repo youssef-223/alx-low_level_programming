d1dbd44efbb27ea15ca98b5a5aa3b03b0dbd5afe
youssef-223 <youssef.moustafa202@gmail.com>
2023-11-09 14:49:12 -0800

    a

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * print_numbers - Short description, single line
+ * @separator: param1
+ * @n: param2
+ * @...: param3
+ *
+ * Description: Description
+ *
+ * Return: Return
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", va_arg(ap, int), i ? (seperator ? seperator : "") : "\n");
+	va_end(ap);
+}
