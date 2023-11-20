27f4bfac3baa9ac4c779fa1ffde623397f5b6ed7
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 07:26:55 -0800

    More Linked List

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,19 +2,18 @@
 
 
 /**
- * print_listint - print linked list
+ * listint_len - print linked list
  * @h: param1
  *
  * Return: Size of list
 */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+/**
+ * listint_len - print linked list
+ * @head: param1
+ * @n: param1
+ * Return: Size of list
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->n = n;
+	if (*head)
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+}
+
