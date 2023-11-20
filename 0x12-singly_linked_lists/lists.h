c3ec01334db6ddb24cce009892b80618381cf7f2
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 05:50:18 -0800

    linked list task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef "LIST_H"
-#define "LIST_H"
+#ifndef LIST_H
+#define LIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,5 +28,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-
-#elif	/* LIST_H */
+#endif	/* LIST_H */
