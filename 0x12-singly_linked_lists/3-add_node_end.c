380fd0bb3f27c6709ed0afd9ded5f6b26ca36aa2
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 06:15:35 -0800

    linked list task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+
+/**
+ * add_node_end - Short description, single line
+ * @head: Description of parameter x
+ * @str: Description of parameter x
+ * Description: Longer description of the function)?
+ *
+ * Return: size of list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!h || !new_node)
+		return (NULL);
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if(!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+/**
+ * free_list - Short description, single line
+ * @head: Description of parameter x
+ * Description: Longer description of the function)?
+ *
+ * Return: size of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node, *next_node;
+
+	if (!head)
+		return;
+
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
+
