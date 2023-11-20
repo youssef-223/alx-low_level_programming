1e9a40a52709301df194361d23d277a3a254a430
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 05:54:15 -0800

    linked list task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * list_len - Short description, single line
+ * @h: Description of parameter x
+ *
+ * Description: Longer description of the function)?
+ *
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
