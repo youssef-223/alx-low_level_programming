58d0391fa9afb9c41c659ba50039e92bb07ca20c
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 06:26:50 -0800

    linked list task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,10 +3,10 @@ section			.text
 		global	main
 
 main:
-		mov		edi , msg
+		mov		edi, msg
 		mpv		eax, 0
 		call	printf
 		
 section		.data
-		msg db 'Hello, Holberton', 0xa, p
+		msg db 'Hello, Holberton', 0xa, 0
 
