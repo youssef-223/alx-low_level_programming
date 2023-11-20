158013dadd102968f7cf8f79128c5b0a444b2b12
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 11:51:21 -0800

    linked list task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -19,7 +19,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node->n = n;
 	if (*head)
 		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+	*head = new_node;
+	return (new_node);
 }
-
