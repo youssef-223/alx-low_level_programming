22d4e1872c4f1d107e91c4596d0a32982b14e647
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 12:21:49 -0800

    linked list task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->next = NULL;
 	new_node->n = n;
 	if (!*head)
-		new_node->next = *head;
+		*head = new_node;
 	else
 	{
 		node = *head;
