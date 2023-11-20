af93721c956ae31010118e6372044d4f5846295e
Youssef <youssef.moustafa202@gmail.com>
2023-11-20 06:19:09 -0800

    linked list task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__((constructor));
+
+/**
+ * _constructor - Short description, single line
+ *
+ * Return: size of list
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
+}
+
