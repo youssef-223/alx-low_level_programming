f8b51761656ddd5e13879ca7124d286410733b94
Youssef <youssef.moustafa202@gmail.com>
2023-11-21 08:54:44 -0800

    linked list task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!idx)
 	{
 		new_node->next = *head;
-		*head = *new_node;
+		*head = new_node;
 		return (new_node);
 	}
 	node = *head;
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,6 +32,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 int sum_listint(listint_t *head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 #endif  /* LIST_H */
                     
