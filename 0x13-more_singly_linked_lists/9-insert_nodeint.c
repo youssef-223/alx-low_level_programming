4f5fb110338f6402032e15366b64ac56aa0e2d8b
Youssef <youssef.moustafa202@gmail.com>
2023-11-21 08:50:30 -0800

    linked list task

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+
+/**
+ * insert_nodeint_at_index - print linked list
+ * @head: param1
+ * @idx: param2
+ * @n: param3
+ *
+ * Return: address of the inserted node
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *new_node = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!idx)
+	{
+		new_node->next = *head;
+		*head = *new_node;
+		return (new_node);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (new_node);
+		}
+		i++;
+		node = node->next;
+	}
+	free(new_node);
+	return (NULL);
+}
