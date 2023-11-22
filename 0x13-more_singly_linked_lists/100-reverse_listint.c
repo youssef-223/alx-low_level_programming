06c9aecb1db5d8363faf96c38b2eac39ee16b758
Youssef <youssef.moustafa202@gmail.com>
2023-11-21 17:13:53 -0800

    linked list task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+/**
+ * reverse_listint - print linked list
+ * @head: param1
+ *
+ * Return: address of the inserted node
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = NULL, *next = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+
+	node = *head;
+	*head = NULL;
+	while (node)
+	{
+		next = node->next;
+		node->next = *head;
+		*head = node;
+		node = next;
+	}
+	return (*head);
+}
