62e8262db067d4b0dc0f02083b8dd6fa270bf8d1
Youssef <youssef.moustafa202@gmail.com>
2023-11-21 17:18:36 -0800

    linked list task

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+/**
+ * find_listint_loop - print linked list
+ * @head: param1
+ *
+ * Return: address of the inserted node
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; pre != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
