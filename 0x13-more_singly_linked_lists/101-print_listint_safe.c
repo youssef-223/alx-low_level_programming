08f9c3a04e7992990027d3dfb102b70cd1167a77
Youssef <youssef.moustafa202@gmail.com>
2023-11-21 17:36:09 -0800

    linked list task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -56,29 +56,3 @@ size_t print_listint_safe(const listint_t *head)
 	return (num);
 }
 
-
-
-
-/**
- * find_listint_loop - print linked list
- * @head: param1
- *
- * Return: address of the inserted node
-*/
-
-listint_t *find_listint_loop(listint_t *head)
-{
-	listint_t *ptr, *end;
-
-	if (head == NULL)
-		return (NULL);
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-		for (ptr = head; pre != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
