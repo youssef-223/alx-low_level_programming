639e06c458c87ddfa2f422761caf95c630f35331
Youssef <youssef.moustafa202@gmail.com>
2023-11-21 17:05:05 -0800

    linked list task

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+
+/**
+ * delete_nodeint_at_index - print linked list
+ * @head: param1
+ * @index: param2
+ *
+ * Return: address of the inserted node
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *prev_node;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node);
+			return (1);
+		}
+		i++
+		prev_node = node;
+		node = node->next;
+	}
+	return (-1);
+}
