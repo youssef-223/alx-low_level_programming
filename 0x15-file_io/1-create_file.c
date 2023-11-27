20aae5908d11b9acb7f2e520cc5d25f341deee39
Youssef <youssef.moustafa202@gmail.com>
2023-11-27 12:30:39 -0800

    linked list task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * create_file - print linked list
+ * @filename: param1
+ * @text_content: param2
+ *
+ * Return: int number
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_TWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
+
