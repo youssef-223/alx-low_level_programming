f8aebb7357c654bf794fb21e86ff8f9f5d36abaa
Youssef <youssef.moustafa202@gmail.com>
2023-11-27 12:35:25 -0800

    linked list task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,24 @@
 #include "main.h"
 
+/**
+ * _strlen - Computes the length of a string.
+ *
+ * @s: The string to measure.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != '\0')
+	{
+		counter++;
+		s++;
+	}
+
+	return (counter);
+}
 
 /**
  * create_file - print linked list
@@ -16,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_TWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
