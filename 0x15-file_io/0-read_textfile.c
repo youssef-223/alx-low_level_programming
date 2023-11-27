3a3b5d56bcd96456d6bbb53adf7ce34cbadf3e24
Youssef <youssef.moustafa202@gmail.com>
2023-11-27 12:21:14 -0800

    linked list task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * read_textfile - print linked list
+ * @filename: param1
+ * @letters: param2
+ *
+ * Return: int number
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0]. letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
+}
+
